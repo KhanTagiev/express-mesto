682f37eb529416166d3c3773c9f5d8dac8438194
KhanTagiev <xan.2011@yandex.ru>
2021-07-08 19:54:26 +0300

    refactor: routes celebrate validation

--- routes/cards.js
+++ routes/cards.js
@@ -1,35 +1,38 @@
 const cardRouter = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
+const validator = require('validator');
 
 const authMiddleware = require('../middlewares/auth');
 const {
   getCards, createCard, deleteCardId, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
+const urlValidationMethod = require('../utils/url_valid_meth');
+
 cardRouter.get('/cards', authMiddleware, getCards);
 
 cardRouter.post('/cards', [celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required(),
+    link: Joi.string().required().custom(urlValidationMethod),
   }),
 }), authMiddleware], createCard);
 
 cardRouter.delete('/cards/:cardId', [celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().alphanum().length(24),
+    cardId: Joi.string().hex().length(24),
   }).unknown(true),
 }), authMiddleware], deleteCardId);
 
 cardRouter.put('/cards/:cardId/likes', [celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().alphanum().length(24),
+    cardId: Joi.string().hex().length(24),
   }).unknown(true),
 }), authMiddleware], likeCard);
 
 cardRouter.delete('/cards/:cardId/likes', [celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().alphanum().length(24),
+    cardId: Joi.string().hex().length(24),
   }).unknown(true),
 }), authMiddleware], dislikeCard);
 
