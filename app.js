780533e87c58cd711ff387618dbc5fe528bde1cd
KhanTagiev <xan.2011@yandex.ru>
2021-07-08 16:26:41 +0300

    feat: celebrate vailidation

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const { celebrate, Joi, errors } = require('celebrate');
 
 const userRoutes = require('./routes/users');
 const cardRoutes = require('./routes/cards');
@@ -25,12 +26,28 @@ app.use(cookieParser());
 
 app.use('/', userRoutes);
 app.use('/', cardRoutes);
-app.post('/signin', login);
-app.post('/signup', createUser);
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2),
+    avatar: Joi.string(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), createUser);
 
 app.use((req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
 });
+
+app.use(errors());
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
