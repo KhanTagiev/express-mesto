a9f0731f0f69ffc7c757493c62c94030a5972879
KhanTagiev <xan.2011@yandex.ru>
2021-07-08 12:57:14 +0300

    feat: hide password hash

--- controllers/users.js
+++ controllers/users.js
@@ -54,7 +54,7 @@ const getMeProfile = async (req, res) => {
 const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email }).select('+password');
     if (!user) {
       return res.status(ERR_CODE_UN_AUTH).json({ message: 'Переданы некорректные данные пользователя' });
     }
