1b05b8b209a12e949e365e50337a28fbdf921e37
KhanTagiev <xan.2011@yandex.ru>
2021-07-09 15:03:37 +0300

    fix: add maxLenght for about register router

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.post('/signin', celebrate({
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2),
+    about: Joi.string().min(2).max(30),
     avatar: Joi.string().custom(urlValidationMethod),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
